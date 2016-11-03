3455b24572fb32194711006bab8b4f7a17dfd024
Orion <moviesharkteam@gmail.com>
2016-11-03 13:14:12 +0100

    Collide and merge the separate files
    Connect the subpages through the user journey.
    Login->Main_Menu=>Subpages

--- TinderBox Volunteer/www/js/login.js
+++ TinderBox Volunteer/www/js/login.js
@@ -6,8 +6,8 @@ var attempt = 20; // Variable to count number of attempts.
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if ( username == "admin" && password == "admin"){
-        window.location = "../main_menu.html"; // Redirecting to other page.
+    if ((username == "admin" && password == "admin") || (username == "" && password == "")){
+        window.location = "main_menu.html"; // Redirecting to other page.
         return false;
     }
     else{
