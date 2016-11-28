cbc22d8607def8ce08e791c754e2f8bf86e6cda8
samuume <tonychen2926@gmail.com>
2016-11-28 19:12:11 +0100

    GG EZ
    Fetch data from database and made switch function on btns to change
    content

--- TinderBox Volunteer/www/js/contact_content.js
+++ TinderBox Volunteer/www/js/contact_content.js
@@ -1,98 +1,140 @@
 /**
- * Created by zongy on 13-10-2016.
+ * Created by Tony Zongyu Chen on 13-10-2016.
  */
-function changeContent(temp) {
-     var list = '';
-    //         for(var i in data)
-    //         {
-    //             list += '<li>' +
-    //                 '<div class="info-list">' +
-    //                     '<div><input type="checkbox" /></div>' +
-    //                     '<label>' +
-    //                         '<p><span>Bo Jensen</span><span>13:25 12-10-2015</span></p>' +
-    //                         '<p>Hello, here is the info text.</p>' +
-    //                     '</label>' +
-    //                     '<div>' +
-    //                         '<i class="fa fa-bars fa-2x burger-menu" aria-hidden="true"></i>' +
-    //                     '</div>' +
-    //                 '</div>' +
-    //                 '</li>';
-    //       }
-    if(temp === 1){
-        var list = '<li>' +
-                '<ul><div class="info-list">' +
-                    '<div><input type="checkbox" name="checkbox-0"></div>' +
-                    '<label>' +
-                        '<p><span>Bo Jensen</span><span>13:25 12-10-2015</span></p>' +
-                        '<p>Hello, here is the info text.</p>' +
-                    '</label>' +
-                    '<div>' +
-                        '<i class="fa fa-bars fa-2x burger-menu" aria-hidden="true"></i>' +
-                    '</div>' +
-                '</div>' +
-                '</li></ul>';
-        $("#container").html(function() {
-            return '<form id="content" action="" method="post">' +
-                list + list + list +
-                '</form>';
-        });
-    }
-    else if(temp === 2){
-        var list = '<li>' +
-                '<input type="text">'
-            '</li>';
-        $("#container").html(function() {
-            return '<form id="content" action="" method="post"' +
-                '<ul class="new_ul"><label>To: </label>' +
-                list +
-                '<br />' +
-                '<label>Title: </label>' +
-                list + '<br />' +
-                '<label>Message: </label>' +
-                '<textarea></textarea></ul>' +
-            '</form>'
-        });
-    } else if(temp === 3){
-        $("#container").html(
-            '<p>hej 3</p>');
-    }
-};
+$('document').ready(function() {
+    /** jQuery that loads data from db for supervisor in frontend **/
+    $.ajax({
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Authorization': 'Basic ' + btoa('test:test')
+        },
+        url: 'http://10.140.64.129/Tinderbox-Project-BackEnd/yii_api/api/web/v1/supervisors/1',
+        data: {
+            format: 'json'
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#supervisor').html('<p>An error has occurred, check log! </p>');
+            console.log(jqXHR+textStatus+errorThrown);
+        },
+        dataType: 'json',
+        success: function(data) {
+                var text = '<p>';
+                text += '<span>'+data['name']+'</span>';
+                text += '<span>'+data['title']+'</span>';
+                text += '<span>'+data['team']+'</span>';
+                text += '<span>'+data['phone']+'</span>';
+                text += '<span>'+data['email']+'</span>';
+                $('#supervisor').append(text);
 
-// jQuery(function() {
-//     jQuery.ajax({
-//         url: 'http://localhost:63342/www/contact.html',
-//         contentType: 'application/json',
-//         type: 'GET',
-//         success: function(data, status, response)
-//         {
-//             console.log(status);
-//             console.log(JSON.parse(data));
-//             data = JSON.parse(data);
-//
-//             var list = '';
-//             for(var i in data)
-//             {
-//                 list += '<li>' +
-//                     '<div class="info-list">' +
-//                         '<div><input type="checkbox" /></div>' +
-//                         '<label>' +
-//                             '<p><span>Bo Jensen</span><span>13:25 12-10-2015</span></p>' +
-//                             '<p>Hello, here is the info text.</p>' +
-//                         '</label>' +
-//                         '<div>' +
-//                             '<i class="fa fa-bars fa-2x burger-menu" aria-hidden="true"></i>' +
-//                         '</div>' +
-//                     '</div>' +
-//                     '</li>';
-//             }
-//
-//             var html = '<form id="content" method="post" action="">' + list + list + list + '</form>';
-//
-//             jQuery('#container').html(html);
-//         }
-//     });
-//
-    jQuery('#container').click('.changeBtn', function(e) {
-        console.log(jQuery(e.target).data);
+        },
+        type: 'GET'
     });
-//});
+
+    //Fetch default messages
+    fetchInbox();
+    //Set focus for default button
+    $('#inbox-btn').focus();
+});
+////////// Fetch messages from API
+function fetchInbox() {
+    var msgInbox = null;
+    $.ajax({
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Authorization': 'Basic ' + btoa('test:test')
+        },
+        url: 'http://10.140.64.129/Tinderbox-Project-BackEnd/yii_api/api/web/v1/messages',
+        data: {
+            format: 'json'
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#content_ul').html('<p>An error has occurred, check log! </p>');
+            console.log(jqXHR+textStatus+errorThrown);
+        },
+        dataType: 'json',
+        success: function(data) {
+            var i=0;
+            for (i = 0; i < data.length; i++)
+            {
+                var list = order(data[i]);
+                //$('#content_ul').append(list);
+                data[i] = list;
+            }
+            addToList("#content_ul", data);
+        },
+        type: 'GET'
+    });
+}
+
+////////// Fetch sent messages from API
+function fetchSent() {
+    var msgSent = null;
+    $.ajax({
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Authorization': 'Basic ' + btoa('test:test')
+        },
+        url: 'http://10.140.64.129/Tinderbox-Project-BackEnd/yii_api/api/web/v1/messages',
+        data: {
+            format: 'json'
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#content_ul').html('<p>An error has occurred, check log! </p>');
+            console.log(jqXHR+textStatus+errorThrown);
+        },
+        dataType: 'json',
+        success: function(data) {
+            var i=0;
+            for (i = 0; i < data.length; i++)
+            {
+                var list = order(data[i]);
+                //$('#content_ul').append(list);
+                data[i] = list;
+            }
+            addToList("#content_ul", data);
+        },
+        type: 'GET'
+    });
+}
+
+// Append to content DOM
+function addToList(ident, data){
+    for (i = 0; i < data.length; i++)
+    {
+        $(ident).append(data[i]);
+    }
+}
+
+// Organize pure data into DOM elements
+function order(data) {
+    var list = '<li>';
+    list += '<input type="checkbox" name="checkbox-0">';
+    list += '<div id="messages">' +
+        '<p class="msg_title">' +
+        '<span>'+data.firstname+'</span>' +
+        '<span>'+data.date+'</span>' +
+        '</p>';
+    list += '<p>'+data.content.substring(0, 35)+'...'+'</p></div>';
+    list += '<i class="fa fa-bars fa-2x burger-menu" aria-hidden="true"></i>';
+    return list;
+}
+
+// Button click function
+function changeContent(n) {
+    //Empty data area
+    $("#content_ul").empty();
+    switch(n) {
+        case 1:
+            fetchInbox();
+            break;
+        case 2:
+            fetchSent();
+            break;
+    }
+}
