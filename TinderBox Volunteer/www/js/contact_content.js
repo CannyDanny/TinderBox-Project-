1a644dd76eaa0c758ee9cfbb8dd6e55958dbb73a
samuume <tonychen2926@gmail.com>
2016-11-29 14:29:04 +0100

    Ajax updated but have issues
    The css is almost there, really annoyiny! Well, the POST part of the
    message is still not working.

--- TinderBox Volunteer/www/js/contact_content.js
+++ TinderBox Volunteer/www/js/contact_content.js
@@ -120,9 +120,69 @@ function order(data) {
         '<span>'+data.firstname+'</span>' +
         '<span>'+data.date+'</span>' +
         '</p>';
-    list += '<p>'+data.content.substring(0, 35)+'...'+'</p></div>';
+    list += '<p class="msg_content">'+data.content.substring(0, 37)+'...'+'</p></div>';
     list += '<i class="fa fa-bars fa-2x burger-menu" aria-hidden="true"></i>';
     return list;
+}
+
+////////// POST new messages from API
+function postNew()
+{
+    var msgData = setMsg();
+    $.ajax({
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Authorization': 'Basic ' + btoa('test:test'),
+            'Content-Type': "application/json",
+        },
+        url: 'http://10.140.64.129/Tinderbox-Project-Backend/yii_api/api/web/v1/messages',
+        data: JSON.stringify(msgData),
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#content_ul').html('<p>An error has occurred, check log! </p>');
+            console.log(jqXHR+textStatus+errorThrown);
+        },
+        dataType: 'json',
+        success: function() {
+            $("#content_ul").empty;
+            var res = $("<p>").text("Successfully sent, check inbox!");
+            $("#content_ul")
+                .append(res);
+        },
+        method: 'POST'
+    });
+}
+// Set the message fields
+function setMsg(){
+    var mto = 1;
+    var mfrom = 2;
+    var mdate = date();
+    var mcontent = document.getElementById("msg_content").value;
+    var msg =
+        {
+            "firstname": "new",
+            "lastname": "test",
+            "date": "2016-11-29 12:02:37",
+            "content": "Hello World!",
+            "sender": 1,
+            "recipient": 2
+        };
+    return msg;
+}
+
+
+// Organize input fields into DOM elements
+function newMsg() {
+    var list = '<li class="msgNew_to">';
+    list += '<label>To:</label>';
+    list += '<input type="text" value="1" name="msg_email" id="msg_email"></li>';
+    list += '<li class="msgNew_content">';
+    list += '<label>Message:</label>';
+    list += '<textarea name="msg_content" id="msg_content"></textarea></li>';
+    list += '<li class="btn_line"><button name="submit" onclick="postNew()" type="submit" id="newMsg_btn">send</button></li>'
+    $("#content_ul")
+        .append(list);
 }
 
 // Button click function
@@ -135,6 +195,9 @@ function changeContent(n) {
             break;
         case 2:
             fetchSent();
+            break;
+        case 3:
+            newMsg();
             break;
     }
 }
