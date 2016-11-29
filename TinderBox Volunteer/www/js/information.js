6f7b83d4900b432646ead82b0eda72de3cc72de0
Sergiu Tesu <tesu_andy_95@yahoo.com>
2016-11-29 13:42:39 +0100

    Revert "Revert "Information js file uploaded""
    This reverts commit a3ddc4b2f8cad0a69b3cd5fed3b051abf24ec98b.

---
+++ TinderBox Volunteer/www/js/information.js
@@ -0,0 +1,27 @@
+$('document').ready(function() {
+    $.ajax({
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Authorization': 'Basic ' + btoa('test:test')
+        },
+        url: 'http://10.140.72.137/yii_api/api/web/v1/informations',
+        data: {
+            format: 'json'
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#set').html('<p>An error has occurred, check log! </p>');
+            console.log(jqXHR+textStatus+errorThrown);
+        },
+        dataType: 'json',
+        success: function(data) {
+            var i=0;
+            for (i = 0; i < data.length; i++) {
+                var content = "<div data-role='collapsible' id='set" + i + "'><h3>" + data[i].title + "</h3><p>" + data[i].content + "</p></div>";
+                $("#set").append( content ).collapsibleset('refresh');
+            }
+        },
+        type: 'GET'
+    });
+});
